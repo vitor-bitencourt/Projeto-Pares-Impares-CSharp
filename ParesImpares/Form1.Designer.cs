1bc4c289b920aa08f400fe6e9e8a804a0573ea2c
Vitor Hugo Bitencourt <vitorbitencourt8486@gmail.com>
2022-01-12 10:21:14 -0300

    Projeto para separar e somar os números pares e ímpares

---
+++ ParesImpares/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ParesImpares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_num = new System.Windows.Forms.TextBox();
+            this.txt_qtdg = new System.Windows.Forms.TextBox();
+            this.txt_somag = new System.Windows.Forms.TextBox();
+            this.txt_qtdp = new System.Windows.Forms.TextBox();
+            this.txt_somap = new System.Windows.Forms.TextBox();
+            this.txt_qtdi = new System.Windows.Forms.TextBox();
+            this.txt_somai = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_result = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_num
+            // 
+            this.txt_num.Location = new System.Drawing.Point(94, 36);
+            this.txt_num.Name = "txt_num";
+            this.txt_num.Size = new System.Drawing.Size(100, 20);
+            this.txt_num.TabIndex = 0;
+            // 
+            // txt_qtdg
+            // 
+            this.txt_qtdg.Location = new System.Drawing.Point(129, 115);
+            this.txt_qtdg.Name = "txt_qtdg";
+            this.txt_qtdg.Size = new System.Drawing.Size(100, 20);
+            this.txt_qtdg.TabIndex = 1;
+            // 
+            // txt_somag
+            // 
+            this.txt_somag.Location = new System.Drawing.Point(401, 115);
+            this.txt_somag.Name = "txt_somag";
+            this.txt_somag.Size = new System.Drawing.Size(100, 20);
+            this.txt_somag.TabIndex = 2;
+            // 
+            // txt_qtdp
+            // 
+            this.txt_qtdp.Location = new System.Drawing.Point(94, 25);
+            this.txt_qtdp.Name = "txt_qtdp";
+            this.txt_qtdp.Size = new System.Drawing.Size(100, 26);
+            this.txt_qtdp.TabIndex = 3;
+            // 
+            // txt_somap
+            // 
+            this.txt_somap.Location = new System.Drawing.Point(94, 68);
+            this.txt_somap.Name = "txt_somap";
+            this.txt_somap.Size = new System.Drawing.Size(100, 26);
+            this.txt_somap.TabIndex = 4;
+            // 
+            // txt_qtdi
+            // 
+            this.txt_qtdi.Location = new System.Drawing.Point(94, 19);
+            this.txt_qtdi.Name = "txt_qtdi";
+            this.txt_qtdi.Size = new System.Drawing.Size(100, 26);
+            this.txt_qtdi.TabIndex = 5;
+            // 
+            // txt_somai
+            // 
+            this.txt_somai.Location = new System.Drawing.Point(94, 68);
+            this.txt_somai.Name = "txt_somai";
+            this.txt_somai.Size = new System.Drawing.Size(100, 26);
+            this.txt_somai.TabIndex = 6;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(262, 29);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(84, 27);
+            this.btn_add.TabIndex = 7;
+            this.btn_add.Text = "Adicionar";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_result
+            // 
+            this.btn_result.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_result.Location = new System.Drawing.Point(363, 28);
+            this.btn_result.Name = "btn_result";
+            this.btn_result.Size = new System.Drawing.Size(84, 27);
+            this.btn_result.TabIndex = 8;
+            this.btn_result.Text = "Resultado";
+            this.btn_result.UseVisualStyleBackColor = true;
+            this.btn_result.Click += new System.EventHandler(this.btn_result_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Quantidade geral";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(317, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Soma geral";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Quantidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Soma";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Quantidade";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Soma";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_qtdp);
+            this.groupBox1.Controls.Add(this.txt_somap);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(56, 187);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pares";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_qtdi);
+            this.groupBox2.Controls.Add(this.txt_somai);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(324, 187);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ímpares";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(465, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_result);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_somag);
+            this.Controls.Add(this.txt_qtdg);
+            this.Controls.Add(this.txt_num);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_num;
+        private System.Windows.Forms.TextBox txt_qtdg;
+        private System.Windows.Forms.TextBox txt_somag;
+        private System.Windows.Forms.TextBox txt_qtdp;
+        private System.Windows.Forms.TextBox txt_somap;
+        private System.Windows.Forms.TextBox txt_qtdi;
+        private System.Windows.Forms.TextBox txt_somai;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ParesImpares/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParesImpares
+{
+ 
+
+    public partial class Form1 : Form
+    {
+        double num, somag, somap, somai, qtdg, qtdp, qtdi;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_qtdg.Clear();
+            txt_qtdi.Clear();
+            txt_qtdp.Clear();
+            txt_somag.Clear();
+            txt_somai.Clear();
+            txt_somap.Clear();
+           
+        }
+
+        private void btn_result_Click(object sender, EventArgs e)
+        {
+            
+            
+            //Exibe os resultados das somas
+
+            txt_somap.Text = Convert.ToString(somap);
+            txt_somai.Text = Convert.ToString(somai);
+
+
+            //Exibe a quantidade dos números
+
+            txt_qtdp.Text = Convert.ToString(qtdp);
+            txt_qtdi.Text = Convert.ToString(qtdi);
+
+
+            //Exibe a quantidade geral
+
+            qtdg = qtdp + qtdi;
+            txt_qtdg.Text = Convert.ToString(qtdg);
+            
+
+            //Exibe a soma geral
+
+            somag = somap + somai;
+            txt_somag.Text = Convert.ToString(somag);
+
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            num = double.Parse(txt_num.Text);
+            
+            if (num % 2 == 0)
+            {
+                //Soma os pares existentes com os novos
+                somap = somap + num;
+                //Conta a quantidade de pares
+                qtdp++;  
+            }
+            else
+            {
+                //Soma os ímpares exixtentes com os novos
+                somai = somai + num;
+                //Conta a quantidade de pares
+                qtdi++;
+            }
+
+            txt_num.Clear();
+             
+        }
+
+    }
+}
